a7b83611f255183f5b9162bd34c59578e7df273f
Prof Luis Vulcanis <100953150+profLuisVulcanis@users.noreply.github.com>
2023-07-27 10:05:42 -0300

    Books Store
    First Settings of the Book Store and also change in folder structure.

---
+++ teste.js
@@ -0,0 +1,18 @@
+//find binary search in javascript.
+// Source: https://stackoverflow.com/a/29018745 .
+function binarySearch(arr, el, compare_fn) {
+    let m = 0;
+    let n = arr.length - 1;
+    while (m <= n) {
+        let k = (n + m) >> 1;
+        let cmp = compare_fn(el, arr[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return ~m;
+}
